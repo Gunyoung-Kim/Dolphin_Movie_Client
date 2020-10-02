ae8648dd24fc467de32fb6f3d84c981422c30b65
GunYoung <gun025bba@naver.com>
2020-10-02 21:16:35 +0900

    Initial Commit

---
+++ DolphinMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DolphinMovie
+//
+//  Created by 김건영 on 2020/10/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
